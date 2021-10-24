5e4c50ad8073ceebdc2b32db70de92b7a2351d46
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2021-10-24 17:13:20 +0300

    ...

--- ldir.asm
+++ ldir.asm
@@ -1,3 +1,19 @@
+ld a, (addr), 30000
+ld a, (bc)
+ld a, (de)
+ld a, (hl)
+ld a, (ix+n), 30000, 5
+ld a, (iy+n), 30000, 5
+ld a, a
+ld a, b
+ld a, c
+ld a, d
+ld a, e
+ld a, h
+ld a, i
+ld a, l
+
+
 ld hl, 30000
 ld de, 16384
 ld bc, 6912
